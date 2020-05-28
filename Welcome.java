bc6fc888b9cdf73de29c50a444779ed768f66a5a
karthik <nallaboluk@gmail.com>
2020-05-28 06:57:46 +0530

    adding wellcome program

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+import java.util.*; 
+public class Welcome {
+
+	public static void main(String[] args) {
+		
+		System.out.println("welcome to Employee Wage Computation program using Classes and objects on master branch");
+		}
+}
+
+
